b26eb25a6935c8a71d595f1dd97fc9497a98167b
udonDaisuke <sato.must.breathe0101@gmail.com>
2023-12-16 12:05:57 +0900

    php更新に伴いテーブルデータを整理

--- gs_bm_table.sql
+++ gs_bm_table.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- ホスト: 127.0.0.1
--- 生成日時: 2023-12-14 23:32:29
+-- 生成日時: 2023-12-16 04:04:54
 -- サーバのバージョン： 10.4.32-MariaDB
 -- PHP のバージョン: 8.2.12
 
@@ -43,20 +43,37 @@ CREATE TABLE `bm_main` (
 INSERT INTO `bm_main` (`id`, `name`, `URL`, `summary`, `postby`, `date`) VALUES
 (1, 'ああ', 'あああ', 'ああああ', 'daisuke', '0000-00-00 00:00:00'),
 (2, 'ああ', 'あああ', 'ああああ', 'daisuke', '0000-00-00 00:00:00'),
-(3, 'ああ', 'あああ', 'ああああ', 'daisuke', '0000-00-00 00:00:00');
-
--- --------------------------------------------------------
-
---
--- テーブルの構造 `bm_review`
---
-
-CREATE TABLE `bm_review` (
-  `id` int(11) NOT NULL,
-  `user_id` varchar(50) NOT NULL,
-  `rating` int(1) NOT NULL,
-  `review` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+(3, 'ああ', 'あああ', 'ああああ', 'daisuke', '0000-00-00 00:00:00'),
+(4, 'ｄｓ', 'ｓｄ', 'ｄｓ', 'daisuke', '0000-00-00 00:00:00'),
+(5, 'あそこのおにく', 'tesrt', 'そんなにうまくない', 'daisuke', '0000-00-00 00:00:00'),
+(6, 'そこのケーキ', 'tesrt', 'まぁまぁうまい', 'daisuke', '0000-00-00 00:00:00'),
+(7, 'おにく', 'oniku.com', 'まぁまぁ', 'daisuke', '0000-00-00 00:00:00'),
+(8, 'そば', 'そなななな', 'さいきょおおう', 'daisuke', '0000-00-00 00:00:00'),
+(9, 'ｚ', 'ｚ', 'ｚ', 'daisuke', '0000-00-00 00:00:00'),
+(10, 'ｓ', 'ｓ', 'ｓ', 'daisuke', '0000-00-00 00:00:00'),
+(11, 'ｄふぉｋｈ', 'ｓｄｌｋｆｊ', 'ｓｄ；ｌｆｊ', 'daisuke', '0000-00-00 00:00:00'),
+(12, 'ｄｌｆｌ', 'ｌｄｆｋｇ', 'ｌｄｆｊ', 'daisuke', '0000-00-00 00:00:00'),
+(13, 'ｓｄ', 'ｓｄ', 'ｓｄ', 'daisuke', '0000-00-00 00:00:00'),
+(14, 'あｓｊｋ', 'あｓｄｌｋｊ', 'あｓｄ；ｊ', 'daisuke', '0000-00-00 00:00:00'),
+(15, 'ｓ', 'ｓ', 'ｓ', 'daisuke', '0000-00-00 00:00:00'),
+(16, 'ｆｓｋｊｈ', 'あｄｊｆｈｆｊ', 'ｋｊｄｓｆｈ', 'daisuke', '0000-00-00 00:00:00'),
+(17, 'alshd', 'kjh', 'kj', 'daisuke', '0000-00-00 00:00:00'),
+(18, 'lksjd', 'lkj', 'lkj', 'daisuke', '0000-00-00 00:00:00'),
+(19, 'lk', 'dfk', 'slkj', 'daisuke', '0000-00-00 00:00:00'),
+(20, 'kkjg', 'kjhk', 'kjh', 'daisuke', '0000-00-00 00:00:00'),
+(21, 'skldfjkl', 'hjkld', 'sd', 'daisuke', '0000-00-00 00:00:00'),
+(22, 'skldfjkl', 'hjkld', 'sdc', 'daisuke', '0000-00-00 00:00:00'),
+(23, 'kljds', 'asd', 'asd', 'daisuke', '0000-00-00 00:00:00'),
+(24, 'kljds', 'asd', 'asd', 'daisuke', '0000-00-00 00:00:00'),
+(25, 'kljsadqs', 'asd', 'asd', 'daisuke', '0000-00-00 00:00:00'),
+(26, 'asd', 'sd', 'sd', 'daisuke', '0000-00-00 00:00:00'),
+(27, 'sklahd', 'lkj', 'lk', 'daisuke', '0000-00-00 00:00:00'),
+(28, 'kjh', 'lkj', 'lk', 'daisuke', '0000-00-00 00:00:00'),
+(29, 'kjh', 'lkj', 'lk;lk', 'daisuke', '0000-00-00 00:00:00'),
+(30, 'kjh', 'lkj', 'lk;lk', 'daisuke', '0000-00-00 00:00:00'),
+(31, 'kjh', 'lkj', 'lk;lk', 'daisuke', '0000-00-00 00:00:00'),
+(32, ';l', ':;l:', ':;l', 'daisuke', '0000-00-00 00:00:00'),
+(33, 'イイね機能', 'むり', 'たどりつけず・・・', 'daisuke', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -89,29 +106,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `user_id`, `pass`, `nickname`) VALUES
-(1, '[value-1]', '[value-2]', '[value-3]'),
-(2, 'gstest', 'passgs', 'gs_taro2023'),
-(3, 'dsato', '1111', 'daisuke'),
-(4, 'dsatoｓ', '$2y$10$8wJQpJND5twW.YEI8a5hEu4DuRBk9FVcHvZ2NtF9VikH9QCCPQjha', 'daisuke'),
-(5, 'test1', '$2y$10$ajL6SdtlAjdly2zF/BQMq.b6pwNVa9BVwEzEu6O1nrdA.plubSMQ2', 'daisuke'),
-(6, 'test2', '$2y$10$8ekIOKKZYfPYeXkIYWvfC.u6OYrlmWkfBq3GtAYPDFgsDSZ0vSMQi', 'daisuke');
-
--- --------------------------------------------------------
-
---
--- テーブルの構造 `user_tag`
---
-
-CREATE TABLE `user_tag` (
-  `user_id` int(11) NOT NULL,
-  `tag_1` varchar(20) NOT NULL,
-  `tag_2` varchar(20) NOT NULL,
-  `tag_3` varchar(20) NOT NULL,
-  `tag_4` varchar(20) NOT NULL,
-  `tag_5` varchar(20) NOT NULL,
-  `tag_6` varchar(20) NOT NULL,
-  `tag_7` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+(6, 'test2', '$2y$10$8ekIOKKZYfPYeXkIYWvfC.u6OYrlmWkfBq3GtAYPDFgsDSZ0vSMQi', 'daisuke'),
+(8, 'daisuke', '$2y$10$.HJ.ne2cfBmPg9M/BzbhSO.ONgHeFic7m4RtkxSLtNx8yYLUFBFSC', 'daisuke');
 
 --
 -- ダンプしたテーブルのインデックス
@@ -130,12 +126,6 @@ ALTER TABLE `user`
   ADD PRIMARY KEY (`id`) USING BTREE;
 
 --
--- テーブルのインデックス `user_tag`
---
-ALTER TABLE `user_tag`
-  ADD KEY `on_user_deleted` (`user_id`);
-
---
 -- ダンプしたテーブルの AUTO_INCREMENT
 --
 
@@ -143,13 +133,13 @@ ALTER TABLE `user_tag`
 -- テーブルの AUTO_INCREMENT `bm_main`
 --
 ALTER TABLE `bm_main`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- テーブルの AUTO_INCREMENT `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
